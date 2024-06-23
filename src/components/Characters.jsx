fac4e9ca557a095094725366a737b4ab90bf7218
Alexis <fkahope@Mac.local>
2024-06-23 12:07:48 +0200

    Project LOTR in process..1

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import { useState } from 'react'
+import React, {useState } from 'react'
 import styles from './movies.module.css'
 
+export default function Characters(props){
 
-export default function Characters(props) {
-  const [char,setChar] = useState('')
+  const[char,setChar] = useState('')
+  const {data} = props
 
-  const {data} = props 
-  
-  const mappedList = data.docs.filter(element => {
-    if(char ==''){
+  const mappedList = data.docs.filter(element =>{
+    if (char == ''){
       return true
     }
     if(element['name'].toLowerCase().includes(char.toLowerCase())){
-      return false
+      return true
     }
+    return false
   })
-
-  return (
+  
+  return(
     <div className={styles.movieDashboard}>
-      <input className={styles.characterSelect} value={char} placeholder='character name' onChange={(e) => {setChar(e.target.value)}}></input>
-      {mappedList.map((char,index) => {
-        const keys = Object.keys(char).filter(element => {
-          if( element == 'name' || element == '_id'){
-            return false
-          }
-          if(!char[element]){
-            return false
-          }
-            return true
-        })
-
-        return(
-          <div key= {index} className={styles.information}>
-            <h2 className='styles.keyVal'>{char.name}</h2>
-              {keys.map(title => {
+      <input className={styles.characterSelect} placeholder="Character" value={char} onChange={(e) =>
+      setChar(e.target.value)}></input>
+      {mappedList.map((char,index) =>{
+          const keys = Object.keys(char).filter(element => {
+            if( element == 'name' || element == '_id'){
+              return false
+            }
+            if(!char[element]){
+              console.log("je tape l'element + " + element['name'])
+              return false
+            }
+              return true
+          })
+        return (
+          <div key={index}>
+            <h2>{char.name}</h2>
+            {keys.map(title => {
                 return(
                 <div key ={title} className={styles.keyVal}>
                   <p>{title}: {char[title]}</p>
